9a58edba98a2197135f848b81e2888ec6d2d4542
pressureless <190361902@qq.com>
2022-11-09 21:18:44 -0500

    Add replace script

---
+++ replace.sh
@@ -0,0 +1,5 @@
+sed -i .bak 's/size_t/int/g' ./include/dec_util.h
+sed -i .bak 's/size_t/int/g' ./include/simplex_subset.h 
+sed -i .bak 's/size_t/int/g' ./include/TriangleMesh.h  
+sed -i .bak 's/size_t/int/g' ./src/dec_util.cpp 
+sed -i .bak 's/size_t/int/g' ./src/TriangleMesh.cpp 
